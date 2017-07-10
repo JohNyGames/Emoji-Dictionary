0b9f2298c53b6b7ed351cba62df6462a70b0c586
John Nyberg <nybergjd@gmail.com>
2017-07-10 14:00:46 -0400

    Nobody steals my first commit

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -3,7 +3,7 @@
 //  Emoji Dictionary
 //
 //  Created by John Nyberg on 7/10/17.
-//  Copyright © 2017 JohnnyGames. All rights reserved.
+//  Copyright © 2017 JohnnyGames. All rights reserved!
 //
 
 import UIKit
